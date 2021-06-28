a453906f21597ca9e6ade3bebb987edac3e5e241
Joe Santos <jaysantos012@gmail.com>
2021-06-28 16:29:52 -0400

    Added the html and very basic css to start. adlready added some JS where it takes the info you add to the input and listenrr the event listner and adds the books below. now working on alerts and to delete the book form the list

---
+++ app.js
@@ -0,0 +1,59 @@
+
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+function UI() { }
+
+
+UI.prototype.addBookToList = function (book) {
+    const list = document.getElementById('book-list');
+    // create tr element
+    const row = document.createElement('tr');
+
+    //  insert cols
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class='delete'>X<a></td>
+    `;
+
+    list.appendChild(row);
+}
+
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+document.getElementById('book-form').addEventListener('submit',
+
+    function (e) {
+        //get form values
+        const title = document.getElementById('title').value,
+            author = document.getElementById('author').value,
+            isbn = document.getElementById('isbn').value
+        //    console.log(title, author, isbn);
+
+
+
+        // Instantiate book
+        const book = new Book(title, author, isbn);
+        // console.log(book)
+
+        //  Instantiate UI
+        const ui = new UI();
+
+        //Add book to list
+        ui.addBookToList(book);
+
+        //  Clear feilds
+        ui.clearFields();
+
+
+        e.preventDefault();
+    });
