2984c69463920f14dc880d02f8a593f1cb9e778d
Joe Santos <jaysantos012@gmail.com>
2021-06-28 23:41:09 -0400

    finsihed all actions. updated it all with ES6 classes. now working with localstorage

--- app.js
+++ app.js
@@ -1,45 +1,67 @@
 
-function Book(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
 
-function UI() { }
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+        // create tr element
+        const row = document.createElement('tr');
 
-
-UI.prototype.addBookToList = function (book) {
-    const list = document.getElementById('book-list');
-    // create tr element
-    const row = document.createElement('tr');
-
-    //  insert cols
-    row.innerHTML = `
+        //  insert cols
+        row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
     <td><a href='#' class='delete'>X<a></td>
     `;
 
-    list.appendChild(row);
+        list.appendChild(row);
+    }
+    showAlert(message, className) {
+        // create div
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+    
+        div.appendChild(document.createTextNode(message));
+    
+        const container = document.querySelector('.container');
+    
+        const form = document.querySelector('#book-form');
+    
+        container.insertBefore(div, form);
+    
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 2000);
+    }
+    deleteBook(target) {
+        if (target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    };
+    clearFields() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
 }
 
-UI.prototype.clearFields = function(){
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    document.getElementById('isbn').value = '';
-}
+
+
 
 document.getElementById('book-form').addEventListener('submit',
-
     function (e) {
         //get form values
         const title = document.getElementById('title').value,
             author = document.getElementById('author').value,
             isbn = document.getElementById('isbn').value
         //    console.log(title, author, isbn);
-
-
 
         // Instantiate book
         const book = new Book(title, author, isbn);
@@ -48,19 +70,27 @@ document.getElementById('book-form').addEventListener('submit',
         //  Instantiate UI
         const ui = new UI();
 
-        if(title === '' || author === '' || isbn === '') {
-            UI.showAlert('Please fill in all fields', 'error')            
+        if (title === '' || author === '' || isbn === '') {
+            ui.showAlert('Please fill in all fields', 'error')
         } else {
-             //Add book to list
-        ui.addBookToList(book);
+            //Add book to list
+            ui.addBookToList(book);
 
-        //  Clear feilds
-        ui.clearFields();
-
+            ui.showAlert('Book Added!', 'success');
+            //  Clear feilds
+            ui.clearFields();
         }
-
-       
-
-
         e.preventDefault();
     });
+
+document.getElementById('book-list').addEventListener('click' || 'touchstart', function (e) {
+    const ui = new UI();
+    ui.deleteBook(e.target);
+
+    ui.showAlert('Book Removed!', 'success');
+
+
+    e.preventDefault();
+})
+
+
